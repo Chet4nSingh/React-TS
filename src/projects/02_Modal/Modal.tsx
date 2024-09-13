7af909b73a5eb91abbe89273b92fa5a35165a838
Chetan Singh <chet4nsingh@gmail.com>
2024-09-13 20:29:43 +0530

    React + TypeScript + TailwindCSS

---
+++ src/projects/02_Modal/Modal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <dialog className="p-8 w-2/5 h-2/3 text-2xl" open>
+        This is a modal window made using <br/> React + TypeScript + Tailwind CSS.
+    </dialog>
+  )
+}
+
+export default Modal;
--- src/projects/02_Modal/ModalContainer.tsx
+++ src/projects/02_Modal/ModalContainer.tsx
@@ -1,6 +1,9 @@
+import Modal from "./Modal";
+
 const ModalContainer: React.FC = () => {
     return <main className="h-screen bg-black text-white">
         <button className="w-2 h-2 p-4 m-16 rounded-full bg-white hover:scale-125 duration-75"></button>
+        <Modal />
     </main>
 }
 
