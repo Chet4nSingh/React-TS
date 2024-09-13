d3d675b5aeacda718dddd4b72d8010c5137b36e6
Chetan Singh <chet4nsingh@gmail.com>
2024-09-13 22:24:41 +0530

    React + TypeScript + TailwindCSS

--- src/projects/02_Modal/Modal.tsx
+++ src/projects/02_Modal/Modal.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-const Modal = () => {
+const Modal: React.FC<{ open: boolean }> = ({ open }) => {
+
+    const dialog = useRef<HTMLDialogElement | null>(null);
+
+    useEffect(() => {
+        if (open && dialog.current) {
+            dialog.current.showModal();
+        }
+    }, [open]);
+
   return (
-    <dialog className="p-8 w-2/5 h-2/3 text-2xl" open>
+    <dialog ref={dialog} className="p-8 w-2/5 h-2/3 text-2xl">
         This is a modal window made using <br/> React + TypeScript + Tailwind CSS.
     </dialog>
   )
--- src/projects/02_Modal/ModalContainer.tsx
+++ src/projects/02_Modal/ModalContainer.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Modal from "./Modal";
 
 const ModalContainer: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
     return <main className="h-screen bg-black text-white">
-        <button className="w-2 h-2 p-4 m-16 rounded-full bg-white hover:scale-125 duration-75"></button>
-        <Modal />
+        <button onClick={() => setIsModalOpen(true)} className="w-2 h-2 p-4 m-16 rounded-full bg-white hover:scale-125 duration-75"></button>
+        <Modal open={isModalOpen} />
     </main>
 }
 
