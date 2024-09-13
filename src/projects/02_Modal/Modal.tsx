150d1aa56e0a819de30140a8e19262b4170f5eef
Chetan Singh <chet4nsingh@gmail.com>
2024-09-13 22:44:59 +0530

    React + TypeScript + TailwindCSS

--- src/projects/02_Modal/Modal.tsx
+++ src/projects/02_Modal/Modal.tsx
@@ -1,20 +1,37 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from "react";
 
-const Modal: React.FC<{ open: boolean }> = ({ open }) => {
+const Modal: React.FC<{ open: boolean; closeModal: () => void }> = ({
+  open,
+  closeModal,
+}) => {
+  const dialog = useRef<HTMLDialogElement | null>(null);
 
-    const dialog = useRef<HTMLDialogElement | null>(null);
+  useEffect(() => {
+    if (open && dialog.current) {
+      dialog.current.showModal();
+    }
 
-    useEffect(() => {
-        if (open && dialog.current) {
-            dialog.current.showModal();
-        }
-    }, [open]);
+    return () => {
+        if (dialog.current) dialog.current.close();
+    }
+  }, [open]);
 
   return (
-    <dialog ref={dialog} className="p-8 w-2/5 h-2/3 text-2xl">
-        This is a modal window made using <br/> React + TypeScript + Tailwind CSS.
+    <dialog
+      id="modal"
+      ref={dialog}
+      onClose={closeModal}
+      className="p-8 w-2/5 h-2/3 text-2xl outline-none"
+    >
+      <div className="text-right text-2xl">
+        <button onClick={closeModal}>X</button>
+      </div>
+      <p>
+        This is a modal window made using <br /> React + TypeScript + Tailwind
+        CSS.
+      </p>
     </dialog>
-  )
-}
+  );
+};
 
 export default Modal;
--- src/projects/02_Modal/ModalContainer.tsx
+++ src/projects/02_Modal/ModalContainer.tsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import Modal from "./Modal";
 
 const ModalContainer: React.FC = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    return <main className="h-screen bg-black text-white">
-        <button onClick={() => setIsModalOpen(true)} className="w-2 h-2 p-4 m-16 rounded-full bg-white hover:scale-125 duration-75"></button>
-        <Modal open={isModalOpen} />
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <main className="h-screen bg-black text-white">
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="w-2 h-2 p-4 m-16 rounded-full bg-white outline-none hover:scale-125 duration-75"
+      ></button>
+      <Modal open={isModalOpen} closeModal={() => setIsModalOpen(false)} />
     </main>
-}
+  );
+};
 
 export default ModalContainer;
